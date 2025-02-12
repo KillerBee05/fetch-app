2006ab6c251c668b715374af29cb7525bff100ed
Higher Thought <higherthought@Highers-MacBook-Pro.local>
2025-02-12 07:42:56 -0700

    Update Vite config and start script for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [
     vue()
   ],
@@ -14,13 +14,18 @@ export default defineConfig({
   },
   server: {
     port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
-    host: '0.0.0.0' // Important for deployment
+    host: '0.0.0.0'
   },
   preview: {
     allowedHosts: [
       'fetch-0ql69.kinsta.app'
     ],
     port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
-    host: '0.0.0.0' // Important for deployment
+    host: '0.0.0.0'
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
   }
 })
